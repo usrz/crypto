b18539f411ecc4c419cdfb8f1fabae85008ec47c
Pier Fumagalli <pier@fumagalli.org>
2014-02-04 02:29:35 +0900

    Use BouncyCastle for PKCS7 (internals, not the provider).

--- source/main/org/usrz/libs/crypto/utils/PKCS7.java
+++ source/main/org/usrz/libs/crypto/utils/PKCS7.java
@@ -15,17 +15,26 @@
  * ========================================================================== */
 package org.usrz.libs.crypto.utils;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.security.InvalidKeyException;
-import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.security.Signature;
 import java.security.SignatureException;
+import java.security.cert.Certificate;
 import java.security.cert.X509Certificate;
 import java.security.interfaces.RSAPrivateKey;
-import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import org.bouncycastle.cert.jcajce.JcaCertStore;
+import org.bouncycastle.cms.CMSProcessableByteArray;
+import org.bouncycastle.cms.CMSSignedData;
+import org.bouncycastle.cms.CMSSignedDataGenerator;
+import org.bouncycastle.cms.CMSTypedData;
+import org.bouncycastle.cms.DefaultSignedAttributeTableGenerator;
+import org.bouncycastle.cms.jcajce.JcaSignerInfoGeneratorBuilder;
+import org.bouncycastle.operator.ContentSigner;
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
+import org.bouncycastle.operator.jcajce.JcaDigestCalculatorProviderBuilder;
 
 /**
  * A utility class for managing signatures in <code>PKCS7</code> using
@@ -35,20 +44,6 @@ import java.util.List;
  * @author <a href="mailto:pier@usrz.com">Pier Fumagalli</a>
  */
 public class PKCS7 {
-
-
-    private static final sun.security.x509.AlgorithmId DIGEST_ALGORITHM =     new sun.security.x509.AlgorithmId(sun.security.x509.AlgorithmId.SHA_oid);
-    private static final sun.security.x509.AlgorithmId SIGNATURE_ALGORITHM =  new sun.security.x509.AlgorithmId(sun.security.x509.AlgorithmId.sha1WithRSAEncryption_oid);
-    private static final sun.security.x509.AlgorithmId ENCRYPTION_ALGORITHM = new sun.security.x509.AlgorithmId(sun.security.x509.AlgorithmId.RSAEncryption_oid);
-
-    private static final sun.security.x509.AlgorithmId[] DIGEST_ALGORITHMS = { DIGEST_ALGORITHM };
-
-    private static final sun.security.util.ObjectIdentifier CONTENT_TYPE_OID =   sun.security.pkcs.PKCS9Attribute.CONTENT_TYPE_OID;
-    private static final sun.security.util.ObjectIdentifier MESSAGE_DIGEST_OID = sun.security.pkcs.PKCS9Attribute.MESSAGE_DIGEST_OID;
-    private static final sun.security.util.ObjectIdentifier SIGNING_TIME_OID =   sun.security.pkcs.PKCS9Attribute.SIGNING_TIME_OID;
-    private static final sun.security.util.ObjectIdentifier DATA_OID =   sun.security.pkcs.ContentInfo.DATA_OID;
-
-    /* ====================================================================== */
 
     private PKCS7() {
         throw new IllegalStateException("Do not instantiate");
@@ -74,72 +69,31 @@ public class PKCS7 {
                               final X509Certificate certificate,
                               final List<X509Certificate> authorities,
                               final byte[] data)
-    throws NoSuchAlgorithmException, InvalidKeyException, SignatureException {
+    throws Exception {
+        final ContentSigner signer =
+                new JcaContentSignerBuilder("SHA1withRSA")
+//                    .setProvider(bouncyCastle)
+                    .build(privateKey);
 
-        /* Validate parameters */
-        if (data == null) throw new NullPointerException("Null data");
-        if (privateKey == null) throw new NullPointerException("Null key");
-        if (certificate == null) throw new NullPointerException("Null certificate");
+        final CMSSignedDataGenerator generator = new CMSSignedDataGenerator();
 
-        /* Check the
+        generator.addSignerInfoGenerator(
+                new JcaSignerInfoGeneratorBuilder(
+                        new JcaDigestCalculatorProviderBuilder()
+//                            .setProvider(bouncyCastle)
+                            .build())
+                    .setSignedAttributeGenerator(new DefaultSignedAttributeTableGenerator())
+                    .build(signer, certificate));
 
-        /* Prepare the signature of our data */
-        final byte[] digest = MessageDigest.getInstance(DIGEST_ALGORITHM.getName()).digest(data);
-
-        /* Prepare the set of our authenticated attributes */
-        final sun.security.pkcs.PKCS9Attributes attributes;
-        final byte[] attributesData;
-        try {
-            attributes = new sun.security.pkcs.PKCS9Attributes(
-                        new sun.security.pkcs.PKCS9Attribute[] {
-                            new sun.security.pkcs.PKCS9Attribute(CONTENT_TYPE_OID, DATA_OID),
-                            new sun.security.pkcs.PKCS9Attribute(MESSAGE_DIGEST_OID, digest),
-                            new sun.security.pkcs.PKCS9Attribute(SIGNING_TIME_OID, new Date())
-                        });
-            attributesData = attributes.getDerEncoding();
-        } catch (IOException exception) {
-            throw new IllegalStateException("I/O error without I/O", exception);
+        final Set<Certificate> certificates = new HashSet<>();
+        if (authorities != null) {
+            for (Certificate authority: authorities) certificates.add(authority);
         }
+        certificates.add(certificate);
+        generator.addCertificates(new JcaCertStore(certificates));
 
-        /* Prepare our signature */
-        final Signature signature = Signature.getInstance(SIGNATURE_ALGORITHM.getName());
-        signature.initSign(privateKey);
-        signature.update(attributesData);
-
-        /* Prepare our content info (no content "null", detached signature) */
-        final sun.security.pkcs.ContentInfo contentInfo =
-                new sun.security.pkcs.ContentInfo(
-                        sun.security.pkcs.ContentInfo.DATA_OID,
-                        null);
-
-        /* Add up all our certificates in a single array */
-        final int certificatesNumber = authorities == null ? 1 : authorities.size() + 1;
-        final X509Certificate[] certificates = new X509Certificate[certificatesNumber];
-        if (authorities != null) for (int x = 0; x < authorities.size(); x ++)
-            certificates[x + 1] = authorities.get(x);
-        certificates[0] = certificate;
-
-        /* Create our signer information */
-        final sun.security.pkcs.SignerInfo[] signerInfo = {
-                new sun.security.pkcs.SignerInfo(
-                        (sun.security.x509.X500Name) certificate.getIssuerDN(),
-                        certificate.getSerialNumber(),
-                        DIGEST_ALGORITHM,
-                        attributes,
-                        ENCRYPTION_ALGORITHM,
-                        signature.sign(),
-                        null)}; // no unauthenticated attributes
-
-        /* Encode the signature in PKCS7 and return it */
-        final ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-        try {
-            new sun.security.pkcs.PKCS7(DIGEST_ALGORITHMS,
-                                        contentInfo,
-                                        certificates,
-                                        signerInfo).encodeSignedData(bytes);
-            return bytes.toByteArray();
-        } catch (IOException exception) {
-            throw new IllegalStateException("I/O error without I/O", exception);
-        }
+        final CMSTypedData cmsData = new CMSProcessableByteArray(data);
+        final CMSSignedData signeddata = generator.generate(cmsData, false);
+        return signeddata.getEncoded();
     }
 }
