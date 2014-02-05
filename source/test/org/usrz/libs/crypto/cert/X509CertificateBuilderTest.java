3229dca530a04af858b44cef13b6e8b431c11a1e
Pier Fumagalli <pier@fumagalli.org>
2014-02-05 19:28:53 +0900

    More testing of CA stuff.

--- source/test/org/usrz/libs/crypto/cert/X509CertificateBuilderTest.java
+++ source/test/org/usrz/libs/crypto/cert/X509CertificateBuilderTest.java
@@ -31,6 +31,7 @@ import org.usrz.libs.crypto.cert.X509CertificateBuilder.Mode;
 import org.usrz.libs.crypto.codecs.HexCodec;
 import org.usrz.libs.testing.AbstractTest;
 import org.usrz.libs.testing.Exec;
+import org.usrz.libs.testing.Exec.CapturingProcess;
 import org.usrz.libs.testing.IO;
 
 public class X509CertificateBuilderTest extends AbstractTest {
@@ -108,9 +109,11 @@ public class X509CertificateBuilderTest extends AbstractTest {
 
         final File file = IO.makeTempFile(".der");
         IO.copy(certificate.getEncoded(), file);
-        Exec.exec(new String[] {
-                "openssl", "x509", "-inform", "DER", "-text",
-                "-in", file.getAbsolutePath() });
+        assertEquals(Exec.exec(new String[] {
+                    "openssl", "x509", "-inform", "DER", "-text",
+                    "-in", file.getAbsolutePath() }).waitFor(),
+                0, "OpenSSL failure");
+
     }
 
     @Test
@@ -169,9 +172,11 @@ public class X509CertificateBuilderTest extends AbstractTest {
 
         final File file = IO.makeTempFile(".der");
         IO.copy(certificate.getEncoded(), file);
-        Exec.exec(new String[] {
-                "openssl", "x509", "-inform", "DER", "-text",
-                "-in", file.getAbsolutePath() });
+        IO.copy(certificate.getEncoded(), new File("temp.der"));
+        assertEquals(Exec.exec(new String[] {
+                    "openssl", "x509", "-inform", "DER", "-text",
+                    "-in", file.getAbsolutePath() }).waitFor(),
+                0, "OpenSSL failure");
     }
 
     @Test
@@ -192,16 +197,32 @@ public class X509CertificateBuilderTest extends AbstractTest {
         final X509Certificate authorityCertificate = new X509CertificateBuilder()
                 .selfSigned(authorityPrincipal, authorityKeyPair)
                 .mode(Mode.AUTHORITY)
-                .notBefore(0)
-                .notAfter(1, TimeUnit.DAYS)
                 .crlDistributionPoint("https://usrz.org/test.crl")
                 .build();
 
-//        final File authorityFile = IO.makeTempFile("authority", ".der");
-//        IO.copy(authorityCertificate.getEncoded(), authorityFile);
-//        Exec.exec(new String[] {
-//                "openssl", "x509", "-inform", "DER", "-text",
-//                "-in", authorityFile.getAbsolutePath() });
+        final File authorityFile = IO.makeTempFile("authority", ".der");
+        final File authorityFilePEM = IO.makeTempFile("authority", ".pem");
+        IO.copy(authorityCertificate.getEncoded(), authorityFile);
+        assertEquals(Exec.exec(new String[] {
+                    "openssl", "x509", "-inform", "DER", "-text",
+                    "-out", authorityFilePEM.getAbsolutePath(),
+                    "-in", authorityFile.getAbsolutePath() }).waitFor(),
+                0, "OpenSSL failure");
+        assertEquals(Exec.exec(new String[] {
+                    "openssl", "x509", "-inform", "DER", "-text", "-noout",
+                    "-in", authorityFile.getAbsolutePath() }).waitFor(),
+                0, "OpenSSL failure");
+
+        assertPurpose("sslclient",    true,  authorityFilePEM, authorityFilePEM);
+        assertPurpose("sslserver",    true,  authorityFilePEM, authorityFilePEM);
+        assertPurpose("nssslserver",  true,  authorityFilePEM, authorityFilePEM);
+        assertPurpose("smimesign",    true,  authorityFilePEM, authorityFilePEM);
+        assertPurpose("smimeencrypt", true,  authorityFilePEM, authorityFilePEM);
+        assertPurpose("crlsign",      true,  authorityFilePEM, authorityFilePEM);
+        assertPurpose("any",          true,  authorityFilePEM, authorityFilePEM);
+        assertPurpose("ocsphelper",   true,  authorityFilePEM, authorityFilePEM);
+
+        /* ================================================================== */
 
         final KeyPair serverKeyPair = new KeyPairBuilder().build();
 
@@ -222,17 +243,81 @@ public class X509CertificateBuilderTest extends AbstractTest {
                 .subject(serverPrincipal)
                 .subjectPublicKey(serverKeyPair.getPublic())
                 .serial(1)
-                .notBefore(0)
-                .notAfter(1, TimeUnit.DAYS)
                 .build();
 
-//        final File serverFile = IO.makeTempFile("server", ".der");
-//        IO.copy(serverCertificate.getEncoded(), serverFile);
-//        Exec.exec(new String[] {
-//                "openssl", "x509", "-inform", "DER", "-text",
-//                "-in", serverFile.getAbsolutePath() });
+        final File serverFile = IO.makeTempFile("server", ".der");
+        final File serverFilePEM = IO.makeTempFile("server", ".pem");
+        IO.copy(serverCertificate.getEncoded(), serverFile);
+        assertEquals(Exec.exec(new String[] {
+                    "openssl", "x509", "-inform", "DER", "-text",
+                    "-out", serverFilePEM.getAbsolutePath(),
+                    "-in", serverFile.getAbsolutePath() }).waitFor(),
+                0, "OpenSSL failure");
+        assertEquals(Exec.exec(new String[] {
+                    "openssl", "x509", "-inform", "DER", "-text", "-noout",
+                    "-in", serverFile.getAbsolutePath() }).waitFor(),
+                0, "OpenSSL failure");
+
+        assertPurpose("sslclient",    false, authorityFilePEM, serverFilePEM);
+        assertPurpose("sslserver",    true,  authorityFilePEM, serverFilePEM);
+        assertPurpose("nssslserver",  true,  authorityFilePEM, serverFilePEM);
+        assertPurpose("smimesign",    false, authorityFilePEM, serverFilePEM);
+        assertPurpose("smimeencrypt", false, authorityFilePEM, serverFilePEM);
+        assertPurpose("crlsign",      false, authorityFilePEM, serverFilePEM);
+        assertPurpose("any",          true,  authorityFilePEM, serverFilePEM);
+        assertPurpose("ocsphelper",   true,  authorityFilePEM, serverFilePEM);
+
+        /* ================================================================== */
+
+        final KeyPair clientKeyPair = new KeyPairBuilder().build();
+
+        final X500Principal clientPrincipal = new X500PrincipalBuilder()
+                .country("JP")
+                .state("Tokyo")
+                .locality("Shinjuku")
+                .organisation("USRZ.org")
+                .organisationalUnit("Automated Tests")
+                .commonName("John Doe")
+                .emailAddress("noreply@usrz.org")
+                .build();
+
+        final X509Certificate clientCertificate = new X509CertificateBuilder()
+                .mode(Mode.CLIENT)
+                .issuer(authorityCertificate)
+                .issuerPrivateKey(authorityKeyPair.getPrivate())
+                .subject(clientPrincipal)
+                .subjectPublicKey(clientKeyPair.getPublic())
+                .serial(1)
+                .build();
+
+        final File clientFile = IO.makeTempFile("client", ".der");
+        final File clientFilePEM = IO.makeTempFile("client", ".pem");
+        IO.copy(clientCertificate.getEncoded(), clientFile);
+        assertEquals(Exec.exec(new String[] {
+                    "openssl", "x509", "-inform", "DER", "-text",
+                    "-out", clientFilePEM.getAbsolutePath(),
+                    "-in", clientFile.getAbsolutePath() }).waitFor(),
+                0, "OpenSSL failure");
+        assertEquals(Exec.exec(new String[] {
+                    "openssl", "x509", "-inform", "DER", "-text", "-noout",
+                    "-in", clientFile.getAbsolutePath() }).waitFor(),
+                0, "OpenSSL failure");
+
+        assertPurpose("sslclient",    true,  authorityFilePEM, clientFilePEM);
+        assertPurpose("sslserver",    false, authorityFilePEM, clientFilePEM);
+        assertPurpose("nssslserver",  false, authorityFilePEM, clientFilePEM);
+        assertPurpose("smimesign",    false, authorityFilePEM, clientFilePEM);
+        assertPurpose("smimeencrypt", false, authorityFilePEM, clientFilePEM);
+        assertPurpose("crlsign",      false, authorityFilePEM, clientFilePEM);
+        assertPurpose("any",          true,  authorityFilePEM, clientFilePEM);
+        assertPurpose("ocsphelper",   true,  authorityFilePEM, clientFilePEM);
+
+        /* ================================================================== */
 
         assertEquals(serverCertificate.getIssuerX500Principal(),
+                     authorityCertificate.getSubjectX500Principal(),
+                     "Wrong issuer principal");
+        assertEquals(clientCertificate.getIssuerX500Principal(),
                      authorityCertificate.getSubjectX500Principal(),
                      "Wrong issuer principal");
 
@@ -240,9 +325,36 @@ public class X509CertificateBuilderTest extends AbstractTest {
                      authorityCertificate.getExtensionValue("2.5.29.31"),
                      "Wrong CRL distribution list");
 
+        assertEquals(clientCertificate.getExtensionValue("2.5.29.31"),
+                     authorityCertificate.getExtensionValue("2.5.29.31"),
+                     "Wrong CRL distribution list");
+
         assertEquals(HexCodec.HEX.encode(serverCertificate.getExtensionValue("2.5.29.35")).substring(12), // authority key ID minus ASN.1 header
-                  HexCodec.HEX.encode(authorityCertificate.getExtensionValue("2.5.29.14")).substring(8),  // subject key ID minus ASN.1 header
-                  "Key Identifiers Mismatch");
+                     HexCodec.HEX.encode(authorityCertificate.getExtensionValue("2.5.29.14")).substring(8),  // subject key ID minus ASN.1 header
+                     "Key Identifiers Mismatch");
+
+        assertEquals(HexCodec.HEX.encode(clientCertificate.getExtensionValue("2.5.29.35")).substring(12), // authority key ID minus ASN.1 header
+                     HexCodec.HEX.encode(authorityCertificate.getExtensionValue("2.5.29.14")).substring(8),  // subject key ID minus ASN.1 header
+                     "Key Identifiers Mismatch");
+
     }
 
+    /* ====================================================================== */
+
+    private void assertPurpose(String purpose, boolean allowed, File authorityPem, File pem)
+    throws Exception {
+        final String string = verifyPurpose(purpose, authorityPem, pem);
+        if (allowed) assertEquals(string, pem + ": OK", "Purpose \"" + purpose + "\" not allowed");
+        else assertNotEquals(string, pem + ": OK", "Purpose \"" + purpose + "\" must not be allowed");
+    }
+
+    private String verifyPurpose(String purpose, File authorityPem, File pem)
+    throws Exception {
+        final CapturingProcess process = Exec.capture(new String[] {
+                    "openssl", "verify", "-purpose", purpose,
+                    "-CAfile", authorityPem.getAbsolutePath(),
+                    pem.getAbsolutePath() });
+        assertEquals(process.waitFor(), 0, "OpenSSL failure");
+        return process.getOutput().trim() + process.getError().trim();
+    }
 }
