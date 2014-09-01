31a8e8d8c85d35d5171814f54f945f07efc5599a
Pier Fumagalli <ianosh@gmail.com>
2014-09-01 20:09:39 +0900

    No need for config.h in Darwin/Linux.

--- source/native/scrypt_platform.h
+++ source/native/scrypt_platform.h
@@ -1,6 +1,7 @@
 #ifndef _SCRYPT_PLATFORM_H_
 #define	_SCRYPT_PLATFORM_H_
 
+/*
 #if defined(CONFIG_H_FILE)
 #include CONFIG_H_FILE
 #elif defined(HAVE_CONFIG_H)
@@ -8,5 +9,6 @@
 #else
 #error Need either CONFIG_H_FILE or HAVE_CONFIG_H defined.
 #endif
+*/
 
 #endif /* !_SCRYPT_PLATFORM_H_ */
