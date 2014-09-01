ac5713752e9f000abc4f840d428f6749080333a6
Pier Fumagalli <ianosh@gmail.com>
2014-09-01 22:49:46 +0900

    Native code fixup and makefile.

--- source/native/scrypt_jni.c
+++ source/native/scrypt_jni.c
@@ -7,52 +7,85 @@
 #include <jni.h>
 #include "crypto_scrypt.h"
 
-jbyteArray JNICALL scryptN(JNIEnv *env, jclass cls, jbyteArray passwd, jbyteArray salt,
-    jint N, jint r, jint p, jint dkLen)
+void JNICALL scrypt_jni(JNIEnv *env,
+                        jclass cls,
+                        jbyteArray passwd,
+                        jbyteArray salt,
+                        jbyteArray out,
+                        jint offset,
+                        jint length,
+                        jint N,
+                        jint r,
+                        jint p)
 {
-    jint Plen = (*env)->GetArrayLength(env, passwd);
-    jint Slen = (*env)->GetArrayLength(env, salt);
-    jbyte *P = (*env)->GetByteArrayElements(env, passwd, NULL);
-    jbyte *S = (*env)->GetByteArrayElements(env, salt,   NULL);
-    uint8_t *buf = malloc(sizeof(uint8_t) * dkLen);
-    jbyteArray DK = NULL;
 
-    if (P == NULL || S == NULL || buf == NULL) goto cleanup;
+    jint Plen = -1, Slen = -1;
+    jbyte *P = NULL, *S = NULL;
+    uint8_t *buf = NULL;
 
-    if (crypto_scrypt((uint8_t *) P, Plen, (uint8_t *) S, Slen, N, r, p, buf, dkLen)) {
-        jclass e = (*env)->FindClass(env, "java/lang/IllegalArgumentException");
+    /* Check our parameters */
+    if (passwd == NULL || salt == NULL || out == NULL) {
+      jclass e = (*env)->FindClass(env, "java/lang/NullPointerException");
+      (*env)->ThrowNew(env, e, "Invalid parameter passed to native SCrypt");
+      goto cleanup;
+    }
+
+    /* Get our buffers and buffer lengths */
+    Plen = (*env)->GetArrayLength(env, passwd);
+    Slen = (*env)->GetArrayLength(env, salt);
+    P = (*env)->GetByteArrayElements(env, passwd, NULL);
+    S = (*env)->GetByteArrayElements(env, salt,   NULL);
+
+    if (P == NULL || S == NULL)  {
+      jclass e = (*env)->FindClass(env, "java/lang/InternalError");
+      (*env)->ThrowNew(env, e, "Unable to get array elements in native SCrypt");
+      goto cleanup;
+    }
+
+    /* Allocate some memory for our processing */
+    buf = malloc(sizeof(uint8_t) * length);
+
+    if (buf == NULL) {
+      jclass e = (*env)->FindClass(env, "java/lang/OutOfMemoryError");
+      (*env)->ThrowNew(env, e, "Memory allocation for SCrypt failed");
+      goto cleanup;
+    }
+
+    /* Yay! Go ahead */
+    if (crypto_scrypt((uint8_t *) P, Plen, (uint8_t *) S, Slen, N, r, p, buf, length)) {
+        jclass e;
         char *msg;
         switch (errno) {
             case EINVAL:
-                msg = "N must be a power of 2 greater than 1";
+                e = (*env)->FindClass(env, "java/lang/IllegalArgumentException");
+                msg = "Invalid parameters passed to native SCrypt";
                 break;
             case EFBIG:
             case ENOMEM:
+                e = (*env)->FindClass(env, "java/lang/OutOfMemoryError");
                 msg = "Insufficient memory available";
                 break;
             default:
-                msg = "Memory allocation failed";
+                e = (*env)->FindClass(env, "java/lang/IllegalStateException");
+                msg = "Unknown error in native SCrypt";
         }
         (*env)->ThrowNew(env, e, msg);
         goto cleanup;
     }
 
-    DK = (*env)->NewByteArray(env, dkLen);
-    if (DK == NULL) goto cleanup;
-
-    (*env)->SetByteArrayRegion(env, DK, 0, dkLen, (jbyte *) buf);
+    /* Fill the Java array with the result of our process */
+    (*env)->SetByteArrayRegion(env, out, offset, length, (jbyte *) buf);
 
   cleanup:
 
+    /* Cleanup before getting back */
     if (P) (*env)->ReleaseByteArrayElements(env, passwd, P, JNI_ABORT);
     if (S) (*env)->ReleaseByteArrayElements(env, salt,   S, JNI_ABORT);
     if (buf) free(buf);
-
-    return DK;
 }
 
 static const JNINativeMethod methods[] = {
-    { "scryptN", "([B[BIIII)[B", (void *) scryptN }
+    { "scrypt", "([B[B[BIIIII)V", (void *) scrypt_jni }
 };
 
 jint JNI_OnLoad(JavaVM *vm, void *reserved) {
@@ -62,7 +95,7 @@ jint JNI_OnLoad(JavaVM *vm, void *reserved) {
         return -1;
     }
 
-    jclass cls = (*env)->FindClass(env, "com/lambdaworks/crypto/SCrypt");
+    jclass cls = (*env)->FindClass(env, "org/usrz/libs/crypto/kdf/SCryptNativeHelper");
     int r = (*env)->RegisterNatives(env, cls, methods, 1);
 
     return (r == JNI_OK) ? JNI_VERSION_1_6 : -1;
